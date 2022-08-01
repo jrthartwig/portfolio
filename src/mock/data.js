17c157bfe9138c09c99a7728a20027e425e1df15
jrthartwig <jocelynnhartwig@gmail.com>
2022-07-31 21:23:56 -0400

    fix: changes

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,67 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Fort Wayne Nano Dev', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Fort Wayne Nano Dev', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Fort Wayne',
+  name: 'Nano Dev',
+  subtitle: 'Custom Websites and Business Applications',
+  cta: 'Check Out Our Projects',
+  aboutCta: 'Meet the CEO',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'selfie.jpg',
+  paragraphOne:
+    "Hello there, I'm Jocelynn. I run a boutique software consultancy, focused on helping businesses elevate their online presences.",
+  paragraphTwo:
+    'We specialize in marketing websites that help drive sales and build brand awareness, as well as internal business applications.',
+  paragraphThree:
+    'We are a small team of technologists, who are passionate about quality, innovation, and helping businesses grow.',
+  resume: '', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'yeastyboy2.png',
+    title: 'Yeasty Boy Bread Company',
+    info: 'YBBC is a bread baking business that specializes in sourdoughs and focaccias. Currently the YBBC website functions as a marketing and pre order funnel.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://yeastyboybreadco.com/', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'petersbodyshop.png',
+    title: 'Peters Body Shop',
+    info: 'Peters Body Shop is a collision repair shop. Their website is a marketing brochure website with a call to action to schedule an appointment for a repair estimate.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://petersbodyshop.com', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'blog.png',
+    title: 'Personal/Technical Blog',
+    info: 'A blog about my adventures in technology and my other various hobbies.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://blog.jocelynnlearns.com',
+    // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'csk.png',
+    title: 'Comfort Scratch Kitchen',
+    info: 'A marketing website for a homestyle restaurant in South Carolina',
+    info2: '',
+    url: 'https://comfortscratchkitchen.com/',
+    // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +70,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'jocelynn@fwnanodev.com',
 };
 
 // FOOTER DATA
@@ -67,28 +78,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/jocelynn23/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/jrthartwig',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
